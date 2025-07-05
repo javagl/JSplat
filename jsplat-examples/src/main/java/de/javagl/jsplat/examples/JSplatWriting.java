a8c9312276070e8e9b7edc7220c22cd5b8e641db
Marco Hutter <javagl@javagl.de>
2025-07-05 19:09:50 +0200

    Use MutableSplat for unit cube

--- jsplat-examples/src/main/java/de/javagl/jsplat/examples/JSplatWriting.java
+++ jsplat-examples/src/main/java/de/javagl/jsplat/examples/JSplatWriting.java
@@ -8,7 +8,7 @@ package de.javagl.jsplat.examples;
 import java.io.IOException;
 import java.util.List;
 
-import de.javagl.jsplat.Splat;
+import de.javagl.jsplat.MutableSplat;
 
 /**
  * An example showing how to write splat data in different formats
@@ -23,7 +23,7 @@ public class JSplatWriting
      */
     public static void main(String[] args) throws IOException
     {
-        List<Splat> splats = UnitCubeSplats.create();
+        List<MutableSplat> splats = UnitCubeSplats.create();
         
         // Most of what had been done here is now moved to a utility method
         Utils.writeAll(splats, "./data", "unitCube");
