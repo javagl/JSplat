e82761af7a3be3a385f6cd4aa334d48e8a6de3d5
Marco Hutter <javagl@javagl.de>
2025-11-26 19:42:35 +0100

    Handle PLY with double precision positions

--- jsplat-io-ply/src/main/java/de/javagl/jsplat/io/ply/PlySplatReader.java
+++ jsplat-io-ply/src/main/java/de/javagl/jsplat/io/ply/PlySplatReader.java
@@ -37,11 +37,13 @@ import de.javagl.jsplat.MutableSplat;
 import de.javagl.jsplat.SplatListReader;
 import de.javagl.jsplat.Splats;
 import de.javagl.ply.Descriptor;
+import de.javagl.ply.Descriptors;
 import de.javagl.ply.ElementDescriptor;
 import de.javagl.ply.ObjectPlyTarget;
 import de.javagl.ply.ObjectPlyTarget.Handle;
 import de.javagl.ply.PlyReader;
 import de.javagl.ply.PlyReaders;
+import de.javagl.ply.PlyType;
 import de.javagl.ply.PropertyDescriptor;
 
 /**
@@ -58,7 +60,8 @@ public final class PlySplatReader implements SplatListReader
     }
 
     @Override
-    public List<MutableSplat> readList(InputStream inputStream) throws IOException
+    public List<MutableSplat> readList(InputStream inputStream)
+        throws IOException
     {
         PlyReader plyReader = PlyReaders.create();
         Descriptor descriptor = plyReader.readDescriptor(inputStream);
@@ -70,32 +73,55 @@ public final class PlySplatReader implements SplatListReader
         ObjectPlyTarget plyTarget = new ObjectPlyTarget();
         Handle<MutableSplat> h =
             plyTarget.register("vertex", () -> Splats.create(shDegree));
-        
-        // Convert from right-down-front to right-up-front by 
+
+        // Convert from right-down-front to right-up-front by
         // negating the y- and z-component
-        h.withFloat("x", MutableSplat::setPositionX);
-        h.withFloat("y", (s, y) -> s.setPositionY(-y));
-        h.withFloat("z", (s, z) -> s.setPositionZ(-z));
-        
+        // TODO: This Float/Double check is not so pretty, but
+        // a quick solution for the time being.
+        if (isDouble(descriptor, "x"))
+        {
+            h.withDouble("x", (s, x) -> s.setPositionX(x.floatValue()));
+        }
+        else
+        {
+            h.withFloat("x", MutableSplat::setPositionX);
+        }
+        if (isDouble(descriptor, "y"))
+        {
+            h.withDouble("y", (s, y) -> s.setPositionY(-y.floatValue()));
+        }
+        else
+        {
+            h.withFloat("y", (s, y) -> s.setPositionY(-y));
+        }
+        if (isDouble(descriptor, "z"))
+        {
+            h.withDouble("z", (s, z) -> s.setPositionZ(-z.floatValue()));
+        }
+        else
+        {
+            h.withFloat("z", (s, z) -> s.setPositionZ(-z));
+        }        
+
         h.withFloat("f_dc_0", (s, v) -> s.setShX(0, v));
         h.withFloat("f_dc_1", (s, v) -> s.setShY(0, v));
         h.withFloat("f_dc_2", (s, v) -> s.setShZ(0, v));
-        
+
         h.withFloat("opacity", MutableSplat::setOpacity);
         h.withFloat("scale_0", MutableSplat::setScaleX);
         h.withFloat("scale_1", MutableSplat::setScaleY);
         h.withFloat("scale_2", MutableSplat::setScaleZ);
-        
-        // Convert from right-down-front to right-up-front by 
+
+        // Convert from right-down-front to right-up-front by
         // negating the y- and z-component
         // PLY uses scalar-first quaternions
         h.withFloat("rot_0", MutableSplat::setRotationW);
         h.withFloat("rot_1", MutableSplat::setRotationX);
         h.withFloat("rot_2", (s, y) -> s.setRotationY(-y));
         h.withFloat("rot_3", (s, z) -> s.setRotationZ(-z));
-        
+
         // TODO The coordinate system conversion may have to affect
-        // the SHs, but nothing seems to be specified here in a 
+        // the SHs, but nothing seems to be specified here in a
         // way that allows tools and viewers to agree on something.
         for (int d = 0; d < shDimensions - 1; d++)
         {
@@ -111,6 +137,43 @@ public final class PlySplatReader implements SplatListReader
 
         plyReader.readContent(inputStream, plyTarget);
         return splats;
+    }
+
+    /**
+     * Returns whether the specified property is stored in "double".
+     * 
+     * Apparently, some PLY files store the "x", "y", and "z" properties of the
+     * "vertex" elements in "double". This method assumes that the given
+     * descriptor contains a single element descriptor with a property that has
+     * the given name, and returns whether the type of that property is
+     * {@link PlyType#DOUBLE}.
+     * 
+     * @param descriptor The PLY {@link Descriptor}
+     * @param propertyName The property name
+     * @return The result
+     */
+    private static boolean isDouble(Descriptor descriptor, String propertyName)
+    {
+        List<ElementDescriptor> elementDescriptors =
+            descriptor.getElementDescriptors();
+        for (int e = 0; e < elementDescriptors.size(); e++)
+        {
+            ElementDescriptor elementDescriptor = elementDescriptors.get(e);
+            List<PropertyDescriptor> propertyDescriptors =
+                elementDescriptor.getPropertyDescriptors();
+            for (int p = 0; p < propertyDescriptors.size(); p++)
+            {
+                PropertyDescriptor propertyDescriptor =
+                    propertyDescriptors.get(p);
+                String name = propertyDescriptor.getName();
+                if (name.equals(propertyName))
+                {
+                    PlyType type = propertyDescriptor.getType();
+                    return type == PlyType.DOUBLE;
+                }
+            }
+        }
+        return false;
     }
 
     /**
