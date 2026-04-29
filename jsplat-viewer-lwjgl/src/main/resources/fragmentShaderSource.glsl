4bdae245773e71ee1872916b10b598becfbbfc1a
Marco Hutter <javagl@javagl.de>
2026-04-29 13:13:41 +0200

    Extract viewer state handling into own class

--- jsplat-viewer-lwjgl/src/main/resources/fragmentShaderSource.glsl
+++ jsplat-viewer-lwjgl/src/main/resources/fragmentShaderSource.glsl
@@ -56,7 +56,7 @@ void main()
     float opacity = min(0.99f, alpha * exp(power));
     if (opacity < 1.f / 255.f)
         discard;
-    FragColor = vec4(color, opacity);
+    FragColor = vec4(color, 1.0) * opacity;
 
     // handling special shading effect
     if (render_mod == -3)
